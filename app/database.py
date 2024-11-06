f63e8ea69ed1f7d52694db80663a3414f1b896c9
blastre <NadamKrishna@gmail.com>
2024-11-06 12:20:04 +0530

    render database

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://blog_app_twbj_user:fjofJHz8MHCdOZ9a2u6730cwsPrSKu2H@dpg-cslh203tq21c73eiigv0-a/blog_app_twbj"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
